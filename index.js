4891b74720eb410885e9eef6fa72704665572fc1
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-28 15:46:01 -0300

    Add exemplo de funcoes assincronas (com erro)

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('teste');
+/**
+ * bter um usuario
+ * obter o numero de telefone de um usuario a partir de seu ID
+ * obter o endereço do usuário pelo ID
+ */
+
+function obterUsuario() {
+
+    setTimeout(() => {
+
+        return {
+            id: 1,
+            nopme: 'Batata',
+            dataNascimento: new Date(),
+        }
+    }, 1000);
+}
+
+function obterTelefone(idUsuario) {
+    setTimeout(() => {
+
+        return {
+            telefone: '0123132',
+            ddd: 111
+        }
+    }, timeout);
+}
+
+const usuario = obterUsuario();
+//usuario retornando undefined
+console.log(usuario);
